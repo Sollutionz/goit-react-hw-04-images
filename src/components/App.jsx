507564895ff2a7939bb58c38aa7a9eaf5b94bdf7
Sollution <egor.odnokoz007307@gmail.com>
2023-06-23 03:48:55 +0300

    fixed app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,99 +1,109 @@
 import { Searchbar } from './searchbar/Searchbar';
-import { Component, React } from 'react';
+import { React, useEffect, useState } from 'react';
 import { ImageGallery } from './imageGallery/ImageGallery';
 import { Loader } from './loader/Loader';
 import { Button } from './button/Button';
 import { fetchImages } from 'api/imageFinder';
 import { Modal } from './modal/Modal';
 
-export class App extends Component {
-  state = {
-    isLoading: false,
-    query: '',
-    modalImages: null,
-    alt: '',
-    data: [],
-    page: 1,
-    isModal: false,
-  };
+export const App = () => {
+  const [isloading, setIsLoading] = useState(false);
+  const [search, setSearch] = useState('');
+  const [modalImages, setModalImages] = useState(null);
+  const [alt, setAlt] = useState('');
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isModal, setIsModal] = useState(false);
 
-  onSubmit = async query => {
-    await this.setState({ query, page: 1 });
-    this.setState({
-      isLoading: true,
-    });
-    try {
-      await fetchImages(this.state.query, this.state.page).then(
-        ({ images }) => {
-          this.setState({
-            data: images,
-            modalImages: images.modalImages,
-            page: 1,
-          });
+  useEffect(() => {
+    if (!search) return;
+    const getImg = async () => {
+      try {
+        setIsLoading(true);
+        const { images } = await fetchImages(search, page);
+        if (page === 1) {
+          setData(images);
+          setModalImages(images.modalImages);
+        } else {
+          setData(prev => [...prev, ...images]);
+          setModalImages(images.modalImages);
         }
-      );
-    } catch (error) {
-      alert(error.message);
-    }
-    this.setState({
-      isLoading: false
-    });
-  };
-  onLoadMore = async () => {
-    await this.setState(prevState => ({ page: prevState.page + 1 }));
-    this.setState({
-      isLoading: true,
-    });
-    try {
-      await fetchImages(this.state.query, this.state.page).then(
-        ({ images }) => {
-          this.setState(prev => ({
-            data: [...prev.data, ...images],
-            modalImages: images.modalImages,
-          }));
-        }
-      );
-    } catch (error) {
-      alert(error.message);
-    }
-    this.setState({
-      isLoading: false,
-    });
-  };
+      } catch (error) {
+        alert(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getImg();
+  }, [search, page]);
 
-
-  toggleModal = () => {
-    this.setState(prevState => ({ isModal: !prevState.isModal }));
-  };
-
-  getModalImg = (modalImages, alt) => {
-    this.setState({ modalImages, alt });
-  };
-
-  render() {
-    const { data, loading, modalImages, alt, isModal } = this.state;
-
-    return (
-      <>
-        <div className="App">
-          <Searchbar onSubmit={this.onSubmit} />
-          {loading && <Loader />}
-          <ImageGallery
-            modalImages={modalImages}
-            data={data}
-            openModal={this.toggleModal}
-            getModalImg={this.getModalImg}
-          />
-          {isModal && (
-            <Modal
-              modalImages={modalImages}
-              alt={alt}
-              toggleModal={this.toggleModal}
-            />
-          )}
-          {data.length > 11 && <Button onLoadMore={this.onLoadMore} />}
-        </div>
-      </>
-    );
+  function onSubmit(query) {
+    setSearch(query);
+    setPage(1);
   }
-}
+
+  function onLoadMore() {
+    setPage(prev => prev + 1);
+  }
+
+  // const onSubmit = async query => {
+  //   await setPage(1);
+  //   await setSearch(query)
+  //   setIsLoading(true);
+  //   setSearch(query);
+  //   try {
+  //     await fetchImages(search, page).then(({ images }) => {
+  //       setData(images);
+  //       setModalImages(images.modalImages);
+  //     });
+  //   } catch (error) {
+  //     alert(error.message);
+  //   }
+  //   setIsLoading(false);
+  // };
+  // const onLoadMore = async () => {
+  //   await setPage(prev => prev + 1);
+  //   setIsLoading(true);
+  //   try {
+  //     await fetchImages(search, page).then(({ images }) => {
+  //       setData(prevState => [...prevState, ...images]);
+  //       setModalImages(images.modalImages);
+  //     });
+  //   } catch (error) {
+  //     alert(error.message);
+  //   }
+  //   setIsLoading(false);
+  // };
+
+  const toggleModal = () => {
+    setIsModal(prevState => !prevState);
+  };
+
+  const getModalImg = (modalImages, alt) => {
+    setModalImages(modalImages);
+    setAlt(alt);
+  };
+
+  return (
+    <>
+      <div className="App">
+        <Searchbar onSubmit={onSubmit} />
+        {isloading && <Loader />}
+        <ImageGallery
+          modalImages={modalImages}
+          data={data}
+          openModal={toggleModal}
+          getModalImg={getModalImg}
+        />
+        {isModal && (
+          <Modal
+            modalImages={modalImages}
+            alt={alt}
+            toggleModal={toggleModal}
+          />
+        )}
+        {data.length > 11 && <Button onLoadMore={onLoadMore} />}
+      </div>
+    </>
+  );
+};
